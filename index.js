48b0c53c2a7a30975143097d811e0bc528859e33
Lyudmila Reshetyuk <lady.remila@yandex.ru>
2023-11-07 11:20:50 +0300

    checking the operation of queries postman.co

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express')
+/*const path = require("path")*/
 const app = express()
 const port = 3000
 
+/*app.use(express.json())*/
+
 app.get('/', (req, res) => {
-    res.send('Hello world! It is cloudy today!')
+    res.send('Good morning!');
+    /*res.sendFile('index.html', {root: path.join(__dirname, "./static")})*/
 })
+app.get('/hello', (req, res) => {
+    res.send('Hello!');
+})
+app.get('/hi', (req, res) => {
+    res.send('hi! Get request recieved!');
+})
+
+app.post('/hi', (req, res) => {
+    res.send('hi! Post request recieved');
+})
+
+app.put('/hi', (req, res) => {
+    res.send('hi! Put request recieved');
+})
+
+/*
+app.post('/reg-data', (req, res) => {
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>", req.body)
+})
+*/
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
